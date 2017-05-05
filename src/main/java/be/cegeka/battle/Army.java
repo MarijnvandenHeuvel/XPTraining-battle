98933f66b006e313698a11ed2cbcdf0ba1f8b604
MarijnvandenHeuvel <marijn.vandenheuvel@cegeka.com>
2017-05-05 14:09:02 +0200

    US5: part1

---
+++ src/main/java/be/cegeka/battle/Army.java
@@ -0,0 +1,11 @@
+package be.cegeka.battle;
+
+public class Army {
+    public Army EngageInWar(Army otherArmy){
+        //get first soldier of both armies
+        //fight
+        //remove lost soldier from armyList
+        //continue until one army is empty
+        return this;
+    }
+}
